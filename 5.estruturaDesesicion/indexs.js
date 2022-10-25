ff77e62d4af5ca1b2628ff63230952b090debafd
lidia <lidiaastopuma@gmail.com>
2022-10-25 11:11:27 -0500

    ejercicio de martes

---
+++ 5.estruturaDesesicion/indexs.js
@@ -0,0 +1,12 @@
+numeroMes1
+switch(numeroMes)
+console.log("enero")
+break
+case 2:
+    console.log8("febrero")
+break
+case 3:
+    console.log(marzo)
+    break
+    default:
+        console.log("error mes no encontrado")
