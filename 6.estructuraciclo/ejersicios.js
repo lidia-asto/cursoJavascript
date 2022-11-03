b3ebc30ccd79bd4ecfbeaf09e3258d85717fcd9d
lidia <lidiaastopuma@gmail.com>
2022-11-03 10:45:16 -0500

    clase de jueves

---
+++ 6.estructuraciclo/ejersicios.js
@@ -0,0 +1,25 @@
+// existen3
+//averiguar su concepto aplicacion y ejercicios
+//for
+for (let i=0;i<=10;i++){
+    console.log(i)
+}
+//swile
+let eval =true
+let eval2='salir'
+while(eval==true){
+    if (eval2== 'salir'){
+        console.log('adios')
+        eval=false
+    } 
+    console.log('hola')
+    }
+
+ // con la structura de ciglo for 
+ // mostrar los numeros pares entre 1y 20
+ for (let inicia 2;inicial<=20;inicial+=2){
+ console.log.(inicial)
+ }
+ for ()
+ // do while
+ 
