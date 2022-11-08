8c2e99e320488bb3983cb89258cdc72c5b42bbb7
lidia <lidiaastopuma@gmail.com>
2022-11-08 10:45:38 -0500

    operaciones

---
+++ 7.funciones/ejercicios.js
@@ -0,0 +1,33 @@
+// funtion cortavocales(texto){retur cantidadvocales}
+
+//contarVocales('eucalipto')//5
+function vocales (nombre){
+    let texto='eucalipto'
+    if(vocales=="texto")
+    total=eucalipto
+    return vocales
+}
+console.log()
+
+ 
+// funtion calculadora(num1,num2,operacion){retur total}
+
+// function calcular(num1,num2,operacion){
+//     if(operacion=="resta"){
+//         total=num1-num2
+//     }
+// return total 
+// }
+// console.log(calcular(45,12,"resta"))
+//calculadora(45,12,'resta')//33
+
+function calcular (num1,num2,operacion){
+    if(operacion=="suma"){
+        total=num1+num2
+    }
+    if(operacion=="resta"){
+        total=num1+num2
+    }
+    return total
+}
+console.log(calcular(45,12,"resta"))
